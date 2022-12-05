45d000ef9cdba067c8d6be9ac99e055a80b88ca6
Garrett <gflynn@usc.edu>
2022-12-05 09:35:32 -0800

    Update description and name

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# chrome
- A Chrome Extension for the Brains@Play Framework
+# brainsatplay-extension
+The **Brains@Play** browser extension is a collection of tools for creating and sharing **composable applications** on the web.
+
+## Pages
+### Onboarding: Welcome to Brains@Play
+A redirect to the https://brainsatplay.com/workflow page.
+
+### New Tab (TBD): App Studio
+A customizable IDE for ESCode applications
+- Make the editor toggleable and allow specifying what time load on start (e.g. bare editor, loaded project in editor, or just the project)
+
+### Devtools: A Source Explorer for Web Pages
+This extension shows the state of the current ESCode application by hooking into the global ESMonitor inspector. 
+- Soon, this will just be an editor that lets you modify a page and view its properties using an integrated version of the IDE.
+
+### Popup (TBD): Inventory System for Arbitrary Web Pages
+An inventory-like system to determine what you’ll applying to the page
+
+### Options (TBD): Management Interface for the Extension Suite
+Allow for the management of customizations across the extension pages
+
+
+## Support
+Currently, the extension only supports Chromium browsers.
+
+
+## Additional Features
+### Electron (TBD): A Companion Server
+An optional server that lets you add custom backends to websites
---
+++ src/js/contentScript.js
@@ -0,0 +1,48 @@
+const brainsatplayExtensionActivation = () => {
+
+  const ids = [
+    'brainsatplay-injection'
+  ]
+
+  const proxyId = `${ids[0]}-proxy`
+
+  let ports = {}
+
+  const id = chrome.runtime.id
+
+  // Forward messages from injected scripts to the extension pages
+  window.addEventListener('message', function(event) {
+    const message = event.data;
+
+    const isProxy = message.source === id
+    if (event.source !== window) return;  // Only accept messages from the same frame
+    if (typeof message !== 'object' || message === null || (!ids.includes(message.source) && !isProxy)) return; // Only accept messages that we know are ours
+
+    if (isProxy) message.source = ids[0]
+
+    // Relay-Based Commands
+      let port = ports[message.source]
+      if (!port) {
+        port = ports[message.source] = chrome.runtime.connect({name: message.source});
+        port.onMessage.addListener((message) => {
+          window.postMessage({...message, source: 'brainsatplay-content-script-relay'}, '*')
+        });
+      }
+
+      port.postMessage(message)
+  });
+
+  // Inject a script
+  var script = document.createElement('script'); 
+  script.id = proxyId
+  script.src = chrome.runtime.getURL('js/devtools/injected.js');
+  script.type = "module";
+
+  script.setAttribute('data-version', '0.0.0')
+  script.setAttribute('data-id', id)
+
+  const el = document.head||document.documentElement
+  el.appendChild(script);
+}
+
+brainsatplayExtensionActivation()
